8d691d2a77d8b439531b5c72efa268e9104bf18e
Ayman Aznadi <ayman.aznadi@gmail.com>
2021-01-06 16:54:20 +0100

    correct test for scope

--- tests/abstract_scope.test.js
+++ tests/abstract_scope.test.js
@@ -8,21 +8,28 @@ import {
   getBlackPawnAbstractScope,
 } from "../src/game/abstract_scope.js";
 
-test("Test Bishop on 'e4'", () => {
+test("Test Rook on 'e4'", () => {
   const scope = [
-    "b1",
-    "c2",
-    "d3",
-    "f5",
-    "g6",
-    "h7",
-    "h1",
-    "g2",
-    "f3",
-    "d5",
-    "c6",
-    "b7",
-    "a8",
+    ["e5", "e6", "e7", "e8"],
+    ["e3", "e2", "e1"],
+    ["f4", "g4", "h4"],
+    ["d4", "c4", "b4", "a4"],
   ];
-  expect(getBishopAbstractScope("e4").sort()).toEqual(scope.sort());
+  expect(getRookAbstractScope("e4").sort()).toEqual(scope.sort());
+});
+
+test("Test Rook on 'a1'", () => {
+  const scope = [
+    ["a2", "a3", "a4", "a5", "a6", "a7", "a8"],
+    ["b1", "c1", "d1", "e1", "f1", "g1", "h1"],
+  ];
+  expect(getRookAbstractScope("a1").sort()).toEqual(scope.sort());
+});
+
+test("Test Rook on 'h8'", () => {
+  const scope = [
+    ["g8", "f8", "e8", "d8", "c8", "b8", "a8"],
+    ["h7", "h6", "h5", "h4", "h3", "h2", "h1"],
+  ];
+  expect(getRookAbstractScope("h8").sort()).toEqual(scope.sort());
 });
